2ff7fdd782b106af5d289ff66e77b7911b5ca8f5
Ozkan Canli <zkan.canl@gmail.com>
2023-08-02 23:13:25 +0100

    Add antd and tailwindcss

--- packages/admin/app/dashboard/page.tsx
+++ packages/admin/app/dashboard/page.tsx
@@ -1,1 +1,11 @@
+import React from "react";
+import { Button } from "antd";
 
+const Dashboard = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default Dashboard;
