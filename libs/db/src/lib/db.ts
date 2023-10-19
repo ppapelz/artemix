dfb930ef37563d88cd7d529f35f3d19674550fbf
Ozkan Canli <zkan.canl@gmail.com>
2023-10-19 23:33:25 +0100

    Add openai library

--- libs/db/src/lib/db.ts
+++ libs/db/src/lib/db.ts
@@ -8,7 +8,6 @@ export function getSequelizeInstance(): Sequelize {
   if (!sequelizeInstance) {
     const env = process.env.NODE_ENV || 'development';
     const dbConfig = config[env];
-    console.log(dbConfig)
     sequelizeInstance = new Sequelize({...dbConfig, logging: console.log });
     initializeModels(sequelizeInstance);
   }
